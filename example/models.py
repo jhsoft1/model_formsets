d694ed5b5ada16cc1e10d4725c85c005bdb58f16
jensh <jens@petja.de>
2022-03-27 19:24:04 +0200

    1.1 video link added to models.py

--- example/models.py
+++ example/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+# https://www.youtube.com/watch?v=FnZgy-y6hGA A Quick Intro to Model Formsets in Django
+# Pretty Printed
 class Example(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
